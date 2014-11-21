ae533511ea17df4d97100fddbc0e4258dd54eb91
Josh Miles <joshmilesproduction@gmail.com>
2014-11-21 12:54:18 +0000

    Update useful.py

--- useful.py
+++ useful.py
@@ -1,4 +1,16 @@
 import hashlib
 
+#
+# To use, import useful and then use useful.encrypt("text to be encrypted")
+#
 def encrypt(text):
     return hashlib.sha224(text.encode('utf-8')).hexdigest()
+
+#
+# To use, import useful and then use useful.read_file("file location")
+#
+def read_file(file_location):
+    fo = open(file_location, "r+")
+    str = fo.read()
+    fo.close()
+    return str
